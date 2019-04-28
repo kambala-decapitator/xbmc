164f7078be4ad87409f2e4fb54750afcc32fca57
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-04-28 23:50:20 +0200

    [tvOS] xbmc/windowing
    Co-authored-by: kambala-decapitator <decapitator@ukr.net>
    Co-authored-by: Sylvain Cecchetto <cecchetto.sylvain@me.com>

---
+++ xbmc/windowing/tvos/OSScreenSaverTVOS.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (C) 2017-2019 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "../OSScreenSaver.h"
+
+class COSScreenSaverTVOS : public KODI::WINDOWING::IOSScreenSaver
+{
+public:
+  COSScreenSaverTVOS();
+  void Inhibit() override;
+  void Uninhibit() override;
+};
